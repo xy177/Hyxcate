d2635e2e6882ecbd2603975b5ff98d62e7ff910c
ACGaming <4818419+ACGaming@users.noreply.github.com>
2025-11-02 11:44:35 +0100

    Make TCon client events conditional

--- src/main/java/de/ellpeck/nyx/compat/NyxCompatHandler.java
+++ src/main/java/de/ellpeck/nyx/compat/NyxCompatHandler.java
@@ -3,14 +3,20 @@ package de.ellpeck.nyx.compat;
 import de.ellpeck.nyx.Nyx;
 import de.ellpeck.nyx.compat.tconstruct.ConstructsArmory;
 import de.ellpeck.nyx.compat.tconstruct.TinkersConstruct;
+import de.ellpeck.nyx.compat.tconstruct.TinkersConstructClient;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.relauncher.FMLLaunchHandler;
 
 @EventBusSubscriber(modid = Nyx.ID)
 public class NyxCompatHandler {
     public static void preInit() {
         if (Loader.isModLoaded("tconstruct")) {
             TinkersConstruct.registerToolMaterials();
+            if (FMLLaunchHandler.side().isClient()) {
+                MinecraftForge.EVENT_BUS.register(new TinkersConstructClient());
+            }
 
             // Only load Construct's Armory if Tinkers' Construct is also loaded
             if (Loader.isModLoaded("conarm")) {
--- src/main/java/de/ellpeck/nyx/compat/tconstruct/TinkersConstructClient.java
+++ src/main/java/de/ellpeck/nyx/compat/tconstruct/TinkersConstructClient.java
@@ -1,16 +1,12 @@
 package de.ellpeck.nyx.compat.tconstruct;
 
-import de.ellpeck.nyx.Nyx;
 import de.ellpeck.nyx.events.NyxClientEvents;
 import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
 
-@Mod.EventBusSubscriber(modid = Nyx.ID, value = Side.CLIENT)
 public class TinkersConstructClient {
     @SubscribeEvent
-    public static void onModelRegistry(ModelRegistryEvent event) {
+    public void onModelRegistry(ModelRegistryEvent event) {
         NyxClientEvents.registerFluidRenderer(TinkersConstruct.FREZARITE_FLUID);
         NyxClientEvents.registerFluidRenderer(TinkersConstruct.KREKNORITE_FLUID);
         NyxClientEvents.registerFluidRenderer(TinkersConstruct.METEORITE_FLUID);
