9cc3d9f8edb19c1630320453c8ef162b85452cdd
TechnoMysterio <5113920+TechnoMysterio@users.noreply.github.com>
2025-11-16 14:28:21 +0000

    Sounds and events for crystal footsteps.

--- src/main/java/de/ellpeck/nyx/sound/NyxSoundEvents.java
+++ src/main/java/de/ellpeck/nyx/sound/NyxSoundEvents.java
@@ -38,6 +38,7 @@ public enum NyxSoundEvents {
     lightCrystalBreak("light_crystal_break"),
     lightCrystalHit("light_crystal_hit"),
     lightCrystalPlace("light_crystal_place");
+	lightCrystalStep("light_crystal_step");
 
     private final SoundEvent soundEvent;
 
